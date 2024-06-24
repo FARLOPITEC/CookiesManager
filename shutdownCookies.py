e933c8312b9ceae446df3fca2e95aedfd7e35d29
FranciscoF <166758498+QUIENTRAEHIEL0@users.noreply.github.com>
2024-06-24 17:41:16 +0200

    Update shutdownCookies.py
    version path

--- shutdownCookies.py
+++ shutdownCookies.py
@@ -2,8 +2,7 @@ import os
 import time
 from pathlib import Path
 
-
-def obtenerRutaUsuario():
+def getUserPatch():
     return "{}\\".format(Path.home())
 
 def cerrar_aplicaciones():
@@ -13,16 +12,13 @@ def cerrar_aplicaciones():
     print("Aplicaciones cerradas")
 
 def borrar_cookies(userPath):
-    cookies_user = input("¿Quieres borrar las Cookies de inicio de sesión? [S/N]: ").lower()
-    if cookies_user == "s":
-        print("Eliminando cookies...")
+    print("Eliminando cookies...")
+    try:
         files_to_delete = [
-            os.path.join(userPath, "AppData\\ocal\\Vivaldi\\User Data\\Default\\Network\\Cookies-journal"),
-            os.path.join(userPath, "AppData\\Local\\Vivaldi\\User Data\\Default\\Network\\Cookies")
-            
-            #para chrome
-            #C:\Users\franc\AppData\Local\Google\Chrome\User Data\Default\Network
+            os.path.join(userPath, "AppData", "Local", "Vivaldi", "User Data", "Default", "Network", "Cookies-journal"),
+            os.path.join(userPath, "AppData", "Local", "Vivaldi", "User Data", "Default", "Network", "Cookies")
         ]
+        
 
         for file_path in files_to_delete:
             if os.path.exists(file_path):
@@ -30,42 +26,55 @@ def borrar_cookies(userPath):
                 print(f"Archivo {file_path} eliminado.")
             else:
                 print(f"El archivo {file_path} no existe.")
+    except:
+        print("Cookies no encontradas")
+
+
 
 def apagar():
-    shutdown = input("¿Quieres apagar el ordenador? [S/N]: ").lower()
-    if shutdown == "s":
+    print("Apagando...")
+    time.sleep(1)
+    for i in range(3, 0, -1):
+        print(i)
+        time.sleep(1)
+    print("Apagando el ordenador...")
+    if os.name == "nt":
+        os.system("shutdown /s /f /t 0")
+        #hacerlo valido en linux
+    #elif os.name == "posix":
+        #os.system("sudo shutdown -h now")
+    #else:
+       #print("Sistema operativo no soportado para apagar automáticamente.")
+
+while True:
+    #Calculamos la ruta del usuario
+    userPath = getUserPatch()
+    os.system("cls" if os.name == "nt" else "clear")
+    print("1. Apagar ordenador y borrar cookies")
+    print("2. Borrar cookies")
+    print(". Salir")
+    shutdown = input("Que quieres hacer: ").lower()
+
+    if shutdown == "1":
         print("Apagando...")
         time.sleep(1)
-        for i in range(3, 0, -1):
-            print(i)
-            time.sleep(1)
-        print("Apagando el ordenador...")
-        if os.name == "nt":
-            os.system("shutdown /s /f /t 0")
-    elif shutdown == "n":
+        cerrar_aplicaciones()
+        borrar_cookies(userPath)
+        apagar()
+        break
+
+    elif shutdown == "2":
+        print("Borando cookies...")
+        time.sleep(1)
+        cerrar_aplicaciones()
+        borrar_cookies(userPath)
+        break
+
+    elif shutdown == "3":
         print("Saliendo...")
         time.sleep(1)
+        break
+
     else:
-        print("Opción no válida. Inténtalo de nuevo.")
-
-def main():
-    userPath = obtenerRutaUsuario()
-    while True:
-        close_vivaldi = input("¿Quieres apagar el ordenador? [S/N]: ").lower()
-
-        if close_vivaldi == "s":
-            cerrar_aplicaciones()
-            borrar_cookies()
-            apagar()
-            break
-
-        elif close_vivaldi == "n":
-            print("Saliendo...")
-            time.sleep(1)
-            break
-
-        else:
-            print("Opción no válida. Inténtalo de nuevo.")
-
-if __name__ == "__main__":
-    main()
+        input("Opción no válida. Inténtalo de nuevo.")
+        
